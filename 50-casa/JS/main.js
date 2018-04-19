304e43bee7fcf21f2fc592d8b81c83ea41849120
EdwinSPL24 <37194079+EdwinSPL24@users.noreply.github.com>
2018-04-19 09:16:11 -0500

    Update main.js
    Cambio de minúscula a mayúscula el directorio "PAGES"

--- 50-casa/JS/main.js
+++ 50-casa/JS/main.js
@@ -1,6 +1,6 @@
 $("li a").click(function(event){
 	event.preventDefault();
 	if($(this).data('page')!=null){
-		$("#main-container").load('pages/'+$(this).data('page')+'.html');
+		$("#main-container").load('PAGES/'+$(this).data('page')+'.html');
 	}
 });
